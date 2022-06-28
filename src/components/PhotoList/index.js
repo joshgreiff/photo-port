a1c7bfb039bdb3369850ea0f02b8012bb120635c
Josh Greiff <josh@greiff.org>
2022-06-28 18:27:52 -0400

    20.5.5

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -119,12 +119,12 @@ function PhotoList({ category }){
   const toggleModal = (image, i) => {
     // current photo
     setCurrentPhoto({...image, index: i })
-    setIsModalOpen(true)
+    setIsModalOpen(!isModalOpen)
   }
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
